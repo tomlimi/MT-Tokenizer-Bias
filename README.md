49c0381924c586fc1b9664d494f453f1af70189a
Tom556 <t.limisiewicz@gmail.com>
2022-08-09 15:59:15 +0200

    Aligning with awesome_align

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # MT Tokenizer Bias
 We analyze and mitigate gender bias in MT tokenizers.
+
+#Walk through 
+
+1. Follow the instructions at https://github.com/neulab/awesome-align to install awesome_align. 
--- src/word_alignment_utils.py
+++ src/word_alignment_utils.py
@@ -4,24 +4,24 @@ from collections import defaultdict
 import subprocess
 
 
+AWESOME_ALIGN_MODEL = "bert-base-multilingual-cased"
+
+
 def get_lexical_translations(directory, src_lang, tgt_lang, translator='gold'):
     """ Run fast align forward and save the alignment file. Saved aligned words to JSON"""
 
     file_prefix = directory + '/' + src_lang + '-' + tgt_lang + "_" + translator
     tok_file = file_prefix + ".tok"
-    f_aligned_file = file_prefix + ".falign"
-    r_aligned_file = file_prefix + ".ralign"
     aligned_file = file_prefix + ".align"
     json_file = file_prefix + "_lexical_translations.json"
 
-    # Running fast_align
-    # TODO: decide how to align reverse or two way
-
-    # subprocess.call(f"fast_align -i {tok_file} -d -o -v > {f_aligned_file}", shell=True)
-    # subprocess.call(f"fast_align -i {tok_file} -d -o -v -r > {r_aligned_file}", shell=True)
-    # subprocess.call(f"atools -i {f_aligned_file} -j {r_aligned_file} -c grow-diag-final-and > {aligned_file}", shell=True)
-
-    subprocess.call(f"fast_align -i {tok_file} -d -o -v -r > {aligned_file}", shell=True)
+    # Running awesome_align
+    subprocess.run(f"CUDA_VISIBLE_DEVICES=0 awesome-align \
+                    --output_file={aligned_file} \
+                    --model_name_or_path={AWESOME_ALIGN_MODEL} \
+                    --data_file={tok_file} \
+                    --extraction 'softmax' \
+                    --batch_size 32", shell=True)
 
     lexical_translations =[]
     with open(tok_file, 'r') as in_toks, open(aligned_file, 'r') as in_align:
@@ -41,7 +41,7 @@ def get_lexical_translations(directory, src_lang, tgt_lang, translator='gold'):
             lexical_translations.append(lt_line)
 
     lexical_translations = {lt_idx: lt_line for lt_idx, lt_line in enumerate(lexical_translations)}
-    with open(json_file , 'w', encoding='utf8') as json_s:
+    with open(json_file, 'w', encoding='utf8') as json_s:
         json.dump(lexical_translations, json_s, indent=2, ensure_ascii=False)
 
 
