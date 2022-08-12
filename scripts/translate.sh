e5c848340720fd90deb5263b47abe13c64521b29
Tom556 <t.limisiewicz@gmail.com>
2022-08-12 17:04:45 +0200

    translate and align sentences with EasyNMT

---
+++ scripts/translate.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+CUDA_version=10.1
+CUDNN_version=7.6
+CUDA_DIR_OPT=/opt/cuda/$CUDA_version
+if [ -d "$CUDA_DIR_OPT" ] ; then
+  CUDA_DIR=$CUDA_DIR_OPT
+  export CUDA_HOME=$CUDA_DIR
+  export THEANO_FLAGS="cuda.root=$CUDA_HOME,device=gpu,floatX=float32"
+  export PATH=$PATH:$CUDA_DIR/bin
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$CUDA_DIR/cudnn/$CUDNN_version/lib64:$CUDA_DIR/lib64
+  export CPATH=$CUDA_DIR/cudnn/$CUDNN_version/include:$CPATH
+fi
+
+TGT_LANG=$1
+
+source /lnet/work/people/limisiewicz/mt-tokenizer-bias/.virturalenvs/bin/activate
+cd limisiewicz@chaldene:/lnet/work/people/limisiewicz/mt-tokenizer-bias/MT-Tokenizer-Bias || exit
+
+if [ "$TGT_LANG" == "he" ]; then
+  python src/translate_word_align.py --file tatoeba-test-v2021-08-07.eng-heb.txt --tgt_lang $TGT_LANG --translator opus-mt
+elif [ "$TGT_LANG" == "de" ]; then
+  python src/translate_word_align.py --file tatoeba-test-v2021-08-07.deu-eng.txt --tgt_lang $TGT_LANG --source_first True --translator opus-mt
+else
+  echo "Unsupported target language"
+fi
--- src/translate_word_align.py
+++ src/translate_word_align.py
@@ -1,19 +1,20 @@
-from transformers import pipeline
 from mosestokenizer import MosesTokenizer
 import logging
 import argparse
+from easynmt import EasyNMT
+from tqdm import tqdm
 
 from word_alignment_utils import get_lexical_translations
 
 
-def translate_file(dir, file, src_lang, tgt_lang, src_first, translator):
+def translate_file(dir, file, src_lang, tgt_lang, src_first, translator, batch_size=16):
     """ Translate sentences and save in a format ready to be processed by fast_align """
     src_lines = []
 
     in_file = dir + '/' + file
     out_file = dir + '/' + src_lang + '-' + tgt_lang + "_" + translator + ".tok"
 
-    translation_pipeline = pipeline("translation", model=translator)
+    model = EasyNMT(translator)
 
     with open(in_file, 'r') as in_s:
         for line in in_s:
@@ -25,8 +26,11 @@ def translate_file(dir, file, src_lang, tgt_lang, src_first, translator):
                 src_lines.append(split_line[3])
 
     logging.info("Started translating!")
-    output = translation_pipeline(src_lines)
-    out_lines = [out_line['translation_text'] for out_line in output]
+    src_batches = [src_lines[x:x+batch_size] for x in range(0, len(src_lines), batch_size)]
+    out_lines = []
+    for src_batch in tqdm(src_batches):
+        out_lines.extend(model.translate(src_batch, source_lang=src_lang, target_lang=tgt_lang))
+
     logging.info("Translation done!")
 
     src_tokenizer = MosesTokenizer(src_lang)
@@ -49,8 +53,10 @@ if __name__ == '__main__':
     parser.add_argument('--src_lang', type=str, default='en', required=False)
     parser.add_argument('--tgt_lang', type=str, required=True)
     parser.add_argument('--src_first', type=bool)
+    parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--translator', type=str, required=True)
     args = parser.parse_args()
 
-    translate_file(args.dir, args.file, args.src_lang, args.tgt_lang, args.src_first, args.translator)
+    translate_file(args.dir, args.file, args.src_lang, args.tgt_lang, args.src_first,
+                   args.translator, batch_size=args.batch_size)
     get_lexical_translations(args.dir, args.src_lang, args.tgt_lang, translator=args.translator)
